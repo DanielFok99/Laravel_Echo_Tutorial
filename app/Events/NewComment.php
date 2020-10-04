50f3eacb00600d1fdb77750f111260100de948fc
DanielFok99 <43980351+DanielFok99@users.noreply.github.com>
2020-10-05 01:18:23 +0800

    update composer.lock

--- app/Events/NewComment.php
+++ app/Events/NewComment.php
@@ -19,21 +19,12 @@ class NewComment implements ShouldBroadcastNow
 
     public $comment;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
     public function __construct(Comment $comment)
     {
       $this->comment = $comment;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
+
     public function broadcastOn()
     {
         return new PrivateChannel('post.'.$this->comment->post->id);
